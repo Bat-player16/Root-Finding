14718e201d79d8d7592ce99c1745b84831304e2d
petersapiro-mitten <peter@sapiro-mitten.com>
2026-09-04 10:53:46 -0400

    Update bisect_generalize.m
    I fixed the logic for the same sign error, redid the while loop, added tolerances and iteration counter. cleaned up graph as well, added legend and fixed title etc.

--- bisect_generalize.m
+++ bisect_generalize.m
@@ -1,20 +1,27 @@
-%% Definition of the test function and its derivative
+%% Part 2: Generalized bisection method 
+%INPUTS:
+% fun: the function we are computing the root of
+% l_0: left guess
+% r_0: right guess
+% note that f(x_left) and f(x_right) should have different signs
+% dxtol: termination threshold (stop when interval x_right-x_left < dxtol)
+% ftol: termination threshold (stop when abs(f(x_guess))<ftol
+% max_iter: maximum iteration limit
+%OUTPUTS
+% x: estimate for root of fun
+
 test_func01 = @(x) (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
-test_derivative01 = @(x) 3*(x.^2)/100 - 2*x/8 + 2 +(6/2)*cos(x/2+6) - exp(x/6)/6;
+l_0 = -1; 
+r_0 = 20;  
+dxtol = 1e-6;
+ftol = 1e-3;
+max_iter = 1000;
 
-l_0 = -1; % initial guess for bisect and secant 
-r_0 = 70; % initial guess for bisect and secant 
-x_0 = 40; % initial guess for Newtons method 
-tol = 1e-6;
-
-
-
-%Bisection Method 
-bisect_root = bisect(test_func01,l_0,r_0,tol);
+%Call bisection method 
+bisect_root = bisect(test_func01,l_0,r_0,dxtol, ftol, max_iter);
 fprintf('bisection method root: %.4f\n', bisect_root)
 
-
-%plot of the function and zeros 
+%plot of the function and root calculated by bisect method
 plotroots(test_func01,bisect_root) 
 
 %% Plot function 
@@ -29,34 +36,38 @@ plot(root_1,f(root_1),'ro', 'MarkerSize',8)
 axis([-15,40,-50,80])
 xlabel('x (-)');
 ylabel('f(x) (-)');
-title('Test Function 1')
+title('Bisection Method')
+legend('y = 0', 'f(x)', 'root', 'Location', 'best')
 end 
 
 
-%% Part 1 Basic Implementation
-% Use bisection method to make small piece of code we can later generalize
+%define bisection method
+function root = bisect(f, l, r, dxtol,ftol, max_iter)
+% check are l and r different signs
+    if f(l) * f(r) > 0 
+    error ('same sign guess'); 
+    end 
+    iter = 1;
+    m = (l + r) / 2;
 
-function root = bisect(f, l, r, tol)
-% check are l and r different signs 
-    if (f(l) * f(r) ) > 0 
-%    error ('same sign guess'); 
-    end 
-while (r-l)/2 > tol %check to make sure interval is not too small
-    m = (l+r)/2;
-    if f(m) == 0 % find midpoint
-        root = m;
-        return; 
-    end 
+ 
+while iter <= max_iter && (r-l) > dxtol && abs(f(m))>ftol %check to make sure interval is not too small
+   
+   
     % the first rule checks if the left and middle are opposite signs and since
     % (-)*(+) or (+)*(-) < 0 this can check the first rule and then change the
     % right coordinate to the midpoint, otherwise it fulfills the second rule
-    if f(l)*f(m) < 0 
+    m = (l + r) / 2;
+    if f(l) * f(m) < 0 
         r = m;
     else 
         l = m; 
     end 
+    iter = iter + 1;
 end 
 root = (l+r)/2; 
-end 
+
+end
+ 
 
 
