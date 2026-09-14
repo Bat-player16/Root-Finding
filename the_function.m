c73232bc39c2b91e73fe262d578b176feb3673b4
Bat-player16 <146037718+Bat-player16@users.noreply.github.com>
2026-09-13 20:25:26 -0400

    (Secant) Convergence for Quadratic Equation

--- the_function.m
+++ the_function.m
@@ -1,3 +1,7 @@
 function [fval] = the_function(x)
-     fval = (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
+     %fval = (x.^3)/100 - (x.^2)/8 + 2*x + 6*sin(x/2+6) -.7 - exp(x/6);
+     global input_list;
+        input_list(:,end+1) = x;
+        fval = (x-37.879).^2;
+        dfdx = 2*(x-37.879);
  end
